265f86141a89f38692997134e043763e75455c8e
Leonard Ramsey <lr3hj@virginia.edu>
2018-10-31 17:21:01 -0400

    Updated form, added base logic to build request to POST to AWS service

--- mach3/learning/views.py
+++ mach3/learning/views.py
@@ -3,12 +3,20 @@ from __future__ import unicode_literals
 
 from django.http import JsonResponse
 from django.shortcuts import render
+import requests
 
 # Create your views here.
 
 
 def index(request):
     if request.method == 'POST':
-        # hit AWS API w/ urllib
-        return JsonResponse({'message':'Hello World'})
+        # hit AWS API w/ requests
+        label_img = request.POST.get("label_img", "")
+        message = 'Image invalid. Please upload an image of a nutrition label.'
+        if label_img:
+            payload = {'label_img': label_img, 'user': request.user}
+            # response = requests.post('https://httpbin.org/get', params=payload)
+            # check response
+            message = 'Image upload successful.'
+        return JsonResponse({'message': message})
     return render(request, 'general/index.html')
