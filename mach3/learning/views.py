46c7cbe35b8cd2381564af04e6a9368ae333ce08
Leonard Ramsey <lr3hj@virginia.edu>
2018-10-31 16:09:14 -0400

    Added skeletal home page with basic JSON message, to be filled

--- mach3/learning/views.py
+++ mach3/learning/views.py
@@ -1,6 +1,11 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
+from django.http import JsonResponse
 from django.shortcuts import render
 
 # Create your views here.
+
+
+def index(request):
+    return JsonResponse({'message':'Hello World'})
