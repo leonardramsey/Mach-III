5702eb1a91cd9ad9f4c197ffd33b12b90bf56321
Leonard Ramsey <lr3hj@virginia.edu>
2018-11-05 10:46:46 -0500

    Added data reading and info into ML file, updated requirements.txt

--- classifier.py
+++ classifier.py
@@ -3,26 +3,40 @@ import numpy as np
 import matplotlib
 import matplotlib.pyplot as plt
 import pandas as pd
-from sklearn.model_selection import train_test_split
-from pandas.plotting import scatter_matrix
-from sklearn.preprocessing import Imputer
-from sklearn.preprocessing import LabelEncoder
-from sklearn.preprocessing import OneHotEncoder
-from sklearn.pipeline import Pipeline
-from sklearn.preprocessing import StandardScaler
-from sklearn.metrics import precision_score, recall_score, accuracy_score
-from sklearn.metrics import roc_curve
-from sklearn.model_selection import cross_val_predict
+import json
+# from sklearn.model_selection import train_test_split
+# from pandas.plotting import scatter_matrix
+# from sklearn.preprocessing import Imputer
+# from sklearn.preprocessing import LabelEncoder
+# from sklearn.preprocessing import OneHotEncoder
+# from sklearn.pipeline import Pipeline
+# from sklearn.preprocessing import StandardScaler
+# from sklearn.metrics import precision_score, recall_score, accuracy_score
+# from sklearn.metrics import roc_curve
+# from sklearn.model_selection import cross_val_predict
 
 
 
 
 def ml():
     # read in data
-    data = pd.read_json('data.json')
+    label_info = pd.DataFrame(columns=["Calcium", "Carbohydrate", "Cholesterol", "Dietary fiber", "Energy",
+        "Energy from fat", "Fat", "Iron", "NutriScore", "Nutrition score  France", "Potassium", "Proteins",
+        "Salt", "Saturated fat", "Sodium", "Sugars", "Trans fat", "Vitamin A", "Vitamin C (ascorbic acid)",
+        "Vitamin D", "file_name", "nutrition_label_src", "sno", "url"])
+    with open('data.json') as f:
+        data = json.load(f)
+    i = 0
+    for title in data:
+        label_info.loc[i] = pd.Series(data[title])
+    print('----------------------- Original Data + Info -----------------------')
+    print(label_info.head())
 
     # data info
-
+    print(label_info.info())
+    print(label_info.describe())
+    print('----------------------- Columns -----------------------')
+    print(list(label_info))
     # data clean
 
     # data split
