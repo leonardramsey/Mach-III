ffe958cfbf85e6fda252e2fb9bfcd932f51de851
Leonard Ramsey <lr3hj@virginia.edu>
2018-11-18 10:40:32 -0500

    Added camera shot for inputing image, added checkpoint report.

--- mach3/learning/views.py
+++ mach3/learning/views.py
@@ -9,14 +9,27 @@ import requests
 
 
 def index(request):
+    message=""
     if request.method == 'POST':
         # hit AWS API w/ requests
         label_img = request.POST.get("label_img", "")
+        screenshot_img = request.POST.get("screenshot_img_input", "")
         message = 'Image invalid. Please upload an image of a nutrition label.'
-        if label_img:
+        print('screenshot_img')
+        print(screenshot_img)
+        print('label_img')
+        print(label_img)
+        if screenshot_img:
             payload = {'label_img': label_img, 'user': request.user}
             # response = requests.post('https://httpbin.org/get', params=payload)
             # check response
             message = 'Image upload successful.'
+        elif label_img:
+            payload = {'label_img': label_img, 'user': request.user}
+            # response = requests.post('https://httpbin.org/get', params=payload)
+            # check response
+            message = 'Image upload successful.'
+        else:
+            return render(request, 'general/index.html', {'message':message})
         return JsonResponse({'message': message})
-    return render(request, 'general/index.html')
+    return render(request, 'general/index.html', {'message': message})
